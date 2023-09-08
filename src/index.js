435c18a9b78e32735cda2d59d4a632d7b6d6a352
Tsandesh <uchihazoro02@gmail.com>
2023-09-08 23:08:04 +0545

    refactor: removed unused files and routes

--- src/index.js
+++ src/index.js
@@ -1,49 +1,8 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-// import AuthLayout from "layouts/Auth.js";
-// import AdminLayout from "layouts/Admin.js";
-// import useCurrentUser from "hooks/useCurrentUser";
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
-
-// const DashbordRoutes = () => {
-//   <Switch>
-//     <Route path={`/admin`} component={AdminLayout} />
-//   </Switch>
-// };
-
-// const AuthRoutes = () => {
-//   return (
-//     <Switch>
-//       <Route path="/auth" component={AuthLayout} />
-//     </Switch>
-//   );
-// };
-
-// const App = () => {
-//   const { isAuthenticated, user } = useCurrentUser();
-
-//   if (isAuthenticated) {
-//     return <DashbordRoutes />;
-//   } else {
-//     return <AuthRoutes />;
-//   }
-// };
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Redirect, BrowserRouter, Route, Switch } from "react-router-dom";
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
-import RTLLayout from "layouts/RTL.js";
 import PageNotFound from "views/404/PageNotFound";
 
 ReactDOM.render(
@@ -51,7 +10,7 @@ ReactDOM.render(
     <Switch>
       <Route path={`/auth`} component={AuthLayout} />
       <Route path={`/admin`} component={AdminLayout} />
-      <Route path={`/rtl`} component={RTLLayout} />
+
       <Redirect from="/" to="/auth/signin" />
       <Route path="*" element={<PageNotFound />} />
     </Switch>
